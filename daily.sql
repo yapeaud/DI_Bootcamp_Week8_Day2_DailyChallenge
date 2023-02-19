15c1d7cdf4e144da072686415e6ae16fd72578d4
yapeaud <yapoabed@gmail.com>
2023-02-19 05:10:55 +0000

    w8d2daily

---
+++ daily.sql
@@ -0,0 +1,19 @@
+CREATE TABLE FirstTab ( id integer, name VARCHAR(10));
+
+INSERT INTO FirstTab VALUES (5,'Pawan'), (6,'Sharlee'), (7,'Krish'), (NULL,'Avtaar');
+
+SELECT * FROM FirstTab;
+
+CREATE TABLE SecondTab ( id integer );
+
+INSERT INTO SecondTab VALUES (5), (NULL);
+
+SELECT * FROM SecondTab;
+
+SELECT COUNT(*) FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL );-- 0 à la sortie
+
+SELECT COUNT(*) FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 ); -- 2 à la sortie
+ 
+SELECT COUNT(*) FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab );--on aura 0
+
+SELECT COUNT(*) FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL );-- on aura 2
